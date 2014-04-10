0467f3c493b6d4c06c8cdb0d22d74475fda6a188
Dave Wieringa <dwjm@userwise.com>
2014-04-10 15:48:08 -0400

    Initial Commit

---
+++ TopPlaces/TopPlacesAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TopPlacesAppDelegate.h
+//  TopPlaces
+//
+//  Created by David Wieringa on 4/10/14.
+//  Copyright (c) 2014 Userwise Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TopPlacesAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TopPlaces/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TopPlaces
+//
+//  Created by David Wieringa on 4/10/14.
+//  Copyright (c) 2014 Userwise Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TopPlacesAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TopPlacesAppDelegate class]));
+    }
+}
